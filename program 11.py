217b561fa35ec8202691207f832bb7b203acb4c7
Vidhiyalakshmi C <cvidhiyalakshmi@gmail.com>
2026-02-15 17:07:29 +0530

    Add files via upload

---
+++ program 11.py
@@ -0,0 +1,20 @@
+stack = []
+stack.append(10)
+stack.append(20) 
+stack.append(30)  
+
+print("Stack:", stack)    
+print("Peek:", stack[-1])   
+
+stack.pop()                  
+print("After Pop:", stack)
+queue = []
+queue.append(10)   
+queue.append(20)   
+queue.append(30) 
+
+print("Queue:", queue)    
+
+queue.pop(0)                
+print("After Dequeue:", queue)  
+
